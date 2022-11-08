e6f5cd0c511d6c23c134c5ae8e4b02079a207e15
bradfordwagner <wagner.bradford@gmail.com>
2022-11-08 09:39:35 -0500

    indent

--- topic_config_util_internal_test.go
+++ topic_config_util_internal_test.go
@@ -6,98 +6,99 @@ import (
 )
 
 var _ = Describe("TopicConfigUtil", func() {
-	It("test", func() { Expect(true).To(BeTrue()) })
 
-	type args struct {
-		orig, target TopicConfig
-		isBreaking   bool
-	}
-	var test = func(a args) {
-		isBreaking := checkBreakingChange(a.orig, a.target)
-		Expect(isBreaking).To(Equal(a.isBreaking))
-	}
-	It("has different replication factor - breaks", func() {
-		test(args{
-			orig: TopicConfig{
-				Name:              "t1",
-				Partitions:        3,
-				ReplicationFactor: 2,
-				Config:            TopicConfigDetails{},
-				ACLs:              ACLs{},
-			},
-			target: TopicConfig{
-				Name:              "t1",
-				Partitions:        3,
-				ReplicationFactor: 1,
-				Config:            TopicConfigDetails{},
-				ACLs:              ACLs{},
-			},
-			isBreaking: true,
+	Context("checkBreakingChange", func() {
+		type args struct {
+			orig, target TopicConfig
+			isBreaking   bool
+		}
+		var test = func(a args) {
+			isBreaking := checkBreakingChange(a.orig, a.target)
+			Expect(isBreaking).To(Equal(a.isBreaking))
+		}
+		It("has different replication factor - breaks", func() {
+			test(args{
+				orig: TopicConfig{
+					Name:              "t1",
+					Partitions:        3,
+					ReplicationFactor: 2,
+					Config:            TopicConfigDetails{},
+					ACLs:              ACLs{},
+				},
+				target: TopicConfig{
+					Name:              "t1",
+					Partitions:        3,
+					ReplicationFactor: 1,
+					Config:            TopicConfigDetails{},
+					ACLs:              ACLs{},
+				},
+				isBreaking: true,
+			})
 		})
-	})
-	It("has different num partitions - breaks", func() {
-		test(args{
-			orig: TopicConfig{
-				Name:              "t1",
-				Partitions:        3,
-				ReplicationFactor: 2,
-				Config:            TopicConfigDetails{},
-				ACLs:              ACLs{},
-			},
-			target: TopicConfig{
-				Name:              "t1",
-				Partitions:        1,
-				ReplicationFactor: 2,
-				Config:            TopicConfigDetails{},
-				ACLs:              ACLs{},
-			},
-			isBreaking: true,
+		It("has different num partitions - breaks", func() {
+			test(args{
+				orig: TopicConfig{
+					Name:              "t1",
+					Partitions:        3,
+					ReplicationFactor: 2,
+					Config:            TopicConfigDetails{},
+					ACLs:              ACLs{},
+				},
+				target: TopicConfig{
+					Name:              "t1",
+					Partitions:        1,
+					ReplicationFactor: 2,
+					Config:            TopicConfigDetails{},
+					ACLs:              ACLs{},
+				},
+				isBreaking: true,
+			})
 		})
-	})
-	It("has different acls - passes", func() {
-		test(args{
-			orig: TopicConfig{
-				Name:              "t1",
-				Partitions:        3,
-				ReplicationFactor: 2,
-				Config:            TopicConfigDetails{},
-				ACLs: ACLs{
-					Enabled: true,
+		It("has different acls - passes", func() {
+			test(args{
+				orig: TopicConfig{
+					Name:              "t1",
+					Partitions:        3,
+					ReplicationFactor: 2,
+					Config:            TopicConfigDetails{},
+					ACLs: ACLs{
+						Enabled: true,
+					},
 				},
-			},
-			target: TopicConfig{
-				Name:              "t1",
-				Partitions:        3,
-				ReplicationFactor: 2,
-				Config:            TopicConfigDetails{},
-				ACLs: ACLs{
-					Enabled: false,
+				target: TopicConfig{
+					Name:              "t1",
+					Partitions:        3,
+					ReplicationFactor: 2,
+					Config:            TopicConfigDetails{},
+					ACLs: ACLs{
+						Enabled: false,
+					},
 				},
-			},
-			isBreaking: false,
+				isBreaking: false,
+			})
 		})
-	})
-	It("has different retention - passes", func() {
-		test(args{
-			orig: TopicConfig{
-				Name:              "t1",
-				Partitions:        3,
-				ReplicationFactor: 2,
-				Config: TopicConfigDetails{
-					RetentionMS: "10",
+		It("has different retention - passes", func() {
+			test(args{
+				orig: TopicConfig{
+					Name:              "t1",
+					Partitions:        3,
+					ReplicationFactor: 2,
+					Config: TopicConfigDetails{
+						RetentionMS: "10",
+					},
+					ACLs: ACLs{},
 				},
-				ACLs: ACLs{},
-			},
-			target: TopicConfig{
-				Name:              "t1",
-				Partitions:        3,
-				ReplicationFactor: 2,
-				Config: TopicConfigDetails{
-					RetentionMS: "100",
+				target: TopicConfig{
+					Name:              "t1",
+					Partitions:        3,
+					ReplicationFactor: 2,
+					Config: TopicConfigDetails{
+						RetentionMS: "100",
+					},
+					ACLs: ACLs{},
 				},
-				ACLs: ACLs{},
-			},
-			isBreaking: false,
+				isBreaking: false,
+			})
 		})
 	})
 
