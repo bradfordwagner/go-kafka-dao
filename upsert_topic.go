7a8728a05053a4e21c46707c26b6abd808bc2cb8
bradfordwagner <wagner.bradford@gmail.com>
2022-11-08 10:20:04 -0500

    check breaking changes

--- upsert_topic.go
+++ upsert_topic.go
@@ -1,1 +1,33 @@
 package kafka_dao
+
+import (
+	"github.com/Shopify/sarama"
+)
+
+// UpsertTopic - adds a topic to kafka
+func (d *daoImpl) UpsertTopic(t TopicConfig) (err error) {
+	//	setup admin conn
+	err = d.buildAdminConnection()
+	if err != nil {
+		return err
+	}
+
+	ok, existingConfig, err := d.GetTopicConfig(t.Name)
+	if err != nil {
+		return
+	}
+
+	isBreaking, err := existingConfig.checkBreakingChange(t)
+	if ok && isBreaking {
+		return
+	} else if !ok {
+		err = d.admin.Get().CreateTopic(t.Name, t.ConvertToSaramaTopicDetails(), false)
+	} else {
+		err = d.admin.Get().AlterConfig(sarama.TopicResource, t.Name, t.Config.ToConfigMap(), false)
+	}
+	if err != nil {
+		return
+	}
+
+	return
+}
